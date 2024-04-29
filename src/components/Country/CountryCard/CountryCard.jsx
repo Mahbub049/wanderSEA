f5232b91d1ddf76ce22bb04b77f52d640ceb852f
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-04-30 00:08:42 +0600

    transition added to the country cards

--- src/components/Country/CountryCard/CountryCard.jsx
+++ src/components/Country/CountryCard/CountryCard.jsx
@@ -2,7 +2,7 @@ const CountryCard = ({country}) => {
     const {image, country_name, description} = country;
     return (
         <div>
-            <div className="card bg-base-100 h-[700px] shadow-xl poppins">
+            <div className="card bg-base-100 h-[700px] shadow-xl poppins transform hover:scale-105 transition-transform duration-300 hover:cursor-pointer">
                 <figure><img src={image} className="h-[300px] w-full" alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title mb-1 text-3xl font-bold mons">{country_name}</h2>
